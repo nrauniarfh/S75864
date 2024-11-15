2e89232857eefe41aac4540e3ab00046a0fd89fc
auni <auni@172.17.149.6>
2024-11-15 16:02:19 +0800

    This is my third commit for lab 4 activity 3

---
+++ S75864/src/L4q3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author auni
+ */
+import javax.swing.JOptionPane;
+public class L4q3 {
+    
+public class Activity3Labmodule4 {
+
+    
+    public static void main(String[] args) {
+        int currentYear=2024;
+       String input=JOptionPane.showInputDialog("Enter IC number : ");
+       
+       String yearPart=input.substring(0,2);
+       String monthPart=input.substring(2,4);
+       String dayPart=input.substring(4,6);
+       int birthYear=Integer.parseInt(yearPart);
+       int month=Integer.parseInt(monthPart);
+       int day=Integer.parseInt(dayPart);
+       birthYear=birthYear + 2000;
+       int age =currentYear - birthYear;
+       String message=String.format("Birth date %d-%d-%d", day,month,birthYear)+
+       String.format("\nage %d",age);
+       JOptionPane.showMessageDialog(null,message);
+    }
+    
+}
+
+}
