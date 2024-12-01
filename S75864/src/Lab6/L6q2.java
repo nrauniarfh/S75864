1bbcd548283bb05e47f0866b2cb379e399792cb9
auni <auni@172.17.149.6>
2024-12-01 09:56:49 +0800

    lab 6 activity 2

--- S75864/src/Lab6/L6q2.java
+++ S75864/src/Lab6/L6q2.java
@@ -12,9 +12,10 @@ public class L6q2 {
     public static void main (String[]args){
         int kilogram=1;
         double pound=0;
+        
         while (kilogram <200){
             pound=kilogram*2.2;
-            System.out.println(kilogram + " " + pound);
+            System.out.println(kilogram + "\t " +  pound);
             kilogram +=2;
         }
     }
