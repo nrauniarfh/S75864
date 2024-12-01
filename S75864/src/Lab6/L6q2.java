d218756d682bfb09d3230cd1eed30687070d3cb8
auni <auni@172.17.149.6>
2024-12-01 09:44:58 +0800

    lab 6 activity 2

---
+++ S75864/src/Lab6/L6q2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab6;
+
+/**
+ *
+ * @author auni
+ */
+public class L6q2 {
+    public static void main (String[]args){
+        int kilogram=1;
+        double pound=0;
+        while (kilogram <200){
+            pound=kilogram*2.2;
+            System.out.println(kilogram + " " + pound);
+            kilogram +=2;
+        }
+    }
+    
+}
