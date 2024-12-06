d6ef7b75b0c7ad56fbadfd1569a450f7b8aa5826
auni <auni@172.17.149.6>
2024-12-06 15:51:51 +0800

    lab 6 activity 5

---
+++ S75864/src/Lab6/L6q5.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab6;
+
+/**
+ *
+ * @author auni
+ */
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+public class L6q5 {
+    public static void main (String []args){
+        double rent=800.00;
+        DateTimeFormatter get = DateTimeFormatter.ofPattern("dd/MM/yyyy ");
+        
+        int i=1;
+        while (i<=20){
+            int u =1;
+            while (u<=15){
+                int month=1;
+                while (month<=12){
+                    LocalDate date=LocalDate.of(2016,month,1).with(TemporalAdjusters.lastDayOfMonth());
+                    String formattedDate = date.format(get);
+                    
+                    System.out.println("---------------------------------------------------");
+                    System.out.println("Payment Coupon for " + date.getMonth());
+                    System.out.println("Building No : " + i + "\t\t" + "Unit No : " + u );
+                    System.out.println();
+                    System.out.println("Amount of rent : " + rent);
+                    System.out.println("Rent due : " + formattedDate);
+                month++;    
+                }
+            u++;    
+            }
+        i++;    
+        }
+        
+        
+        
+    }
+    
+}
