9085969a329a8b00ff07b38ef1bc6d182a07d6a5
auni <auni@172.17.149.6>
2024-12-08 10:33:59 +0800

    lab 7 activity 1

---
+++ S75864/src/Lab7/L7q1Act3.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author auni
+ */
+public class L7q1Act3 {
+     public static void main (String[]args){
+        int first,second;
+        Scanner get=new Scanner (System.in);
+        do {
+            System.out.print("Enter first integer : ");
+            first =get.nextInt();
+            System.out.print("Enter second integer : ");
+            second =get.nextInt();
+            
+            if (first>=second)
+                System.out.println("Error:First number must be less than second");
+        }
+        while (first>=second);
+        System.out.println("Odd number between " + first + " and " + second + " number  ");
+        int test_number=first;
+    
+       
+        while (test_number<=second){
+            if (test_number%2 !=0){
+                System.out.print(test_number + " ");
+            }    
+            test_number++;
+        }
+        System.out.println();
+        
+        System.out.println("Even number between " + first + " and " + second + " number");
+        test_number=first;
+        while (test_number <=second){
+            if(test_number%2 ==0){
+                System.out.print(test_number + " ");
+            }
+            test_number++;
+        }
+        System.out.println();
+    }
+}
+    
+
